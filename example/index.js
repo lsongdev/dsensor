3db0200210ea8b2e3383f99043e776bd2c336efc
Lsong <song940@gmail.com>
2018-09-12 13:50:37 +0800

    fix display issues

--- example/index.js
+++ example/index.js
@@ -4,7 +4,7 @@ const hc = new HCHO("/dev/cu.SLAB_USBtoUART");
 
 hc.on('message', message => {
   const { VALUE, VH } = message;
-  console.log('HCHO: %smg/m3', (VALUE / VH).toFixed(2));
+  console.log('HCHO: %smg/m3', (VALUE / 10**VH).toFixed(2));
 });
 
 hc.on("open", () => {
